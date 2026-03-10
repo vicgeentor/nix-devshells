e5b945b59b6907db521ff228e3a67025b9e85e1e
vicgeentor <github@vicgeentor.nl>
2026-03-10 11:31:58 +0100

    feat: added agda template

---
+++ src/templates/agda/flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            (pkgs.agda.withPackages (p: [
+              p.standard-library
+            ]))
+          ];
+        };
+      }
+    );
+}
